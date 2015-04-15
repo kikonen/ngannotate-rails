39b14e959891238dce594b2d57cef6c956b5f4ba
kari <mr.kari.ikonen@gmail.com>
2015-04-15 22:17:16 +0300

    Share logic between sprockets 2.x and 3.x processors

--- lib/ngannotate/processor.rb
+++ lib/ngannotate/processor.rb
@@ -1,87 +1,17 @@
 require 'execjs'
 require 'sprockets/processor'
+require_relative 'processor_common'
 
 module Ngannotate
   class Processor < Sprockets::Processor
-    def self.name
-      'Ngannotate::Processor'
-    end
-
-    def prepare
-      return unless need_process?
-      @exec_context = self.class.shared_exec_context
-    end
+    include ProcessorCommon
 
     def evaluate(context, locals)
-      state = need_process? ? :process : (ignore_file? ? :ignore : :skip)
-      ::Rails.logger.info "ng-#{state}: #{@file}"
-      return data unless need_process?
-
-      opt = { add: true }.merge!(parse_ngannotate_options)
-      r = @exec_context.call 'window.annotate', data, opt
-      r['src']
-    end
-
-    private
-
-    def self.shared_exec_context
-      @shared_exec_context ||=
-        begin
-          ::Rails.logger.info "ng-prepare"
-          ngannotate_source = File.open(File.expand_path('../../../vendor/ngannotate.js', __FILE__)).read
-          ExecJS.compile "window = {};" + ngannotate_source
-        end
-    end
-
-    def config
-      ::Rails.configuration.ng_annotate
-    end
-
-    #
-    # Is processing done for current file. This is determined by 3 checks
-    #
-    # - config.ignore_paths
-    # - config.process
-    # - NG_FORCE=true env option
-    #
-    def need_process?
-      force_process = ENV['NG_FORCE'] == 'true'
-      !ignore_file? && (force_process || config.process)
-    end
-
-    #
-    # @return true if current file is ignored
-    #
-    def ignore_file?
-      config.ignore_paths.any? do |p|
-        if p.is_a? Proc
-          p.call(@file)
-        else
-          @file.index(p)
-        end
-      end
-    end
-
-    #
-    # Parse extra options for ngannotate
-    #
-    def parse_ngannotate_options
-      opt = config.options.clone
-
-      if ENV['NG_OPT']
-        opt_str = ENV['NG_OPT']
-        if opt_str
-          opt = Hash[opt_str.split(',').map { |e| e.split('=') }]
-          opt.symbolize_keys!
-        end
-      end
-
-      regexp = ENV['NG_REGEXP'] || config.regexp
-      if regexp
-        opt[:regexp] = regexp
-      end
-
-      opt
+      input = {
+        filename: @file,
+        data: data,
+      }
+      process_data(input)
     end
   end
 end
--- lib/ngannotate/processor3.rb
+++ lib/ngannotate/processor3.rb
@@ -1,13 +1,12 @@
 require 'execjs'
+require_relative 'processor_common'
 
 module Ngannotate
   class Processor
+    include ProcessorCommon
+
     def self.instance
       @instance ||= new
-    end
-
-    def self.name
-      'Ngannotate::Processor'
     end
 
     def self.call(input)
@@ -15,51 +14,7 @@ module Ngannotate
     end
 
     def call(input)
-      data = input[:data]
-      return data if skip
-      prepare
-      opt = { add: true }.merge!(parse_opt)
-      r = @context.call 'window.annotate', data, opt
-      r['src']
-    end
-
-    def prepare
-      return if skip
-      ngannotate_source = File.open(File.join(File.dirname(__FILE__), '../../vendor/ngannotate.js')).read
-      @context = ExecJS.compile "window = {};" + ngannotate_source
-    end
-
-    #
-    # To allow testing assets compile in development environment
-    #
-    # To explicitly force assets compile in development environment
-    #  NG_FORCE=true RAILS_ENV=development bundle exec rake assets:clean assets:precompile
-    # or add to environments/development.rb
-    #  config.ng_annotate.process = true
-    #
-    def force
-      ENV['NG_FORCE'] == 'true'
-    end
-
-    #
-    # Skip processing in environments where it does not make sense.
-    # Override by NG_FORCE=true env variable
-    #
-    def skip
-      !force && !::Rails.configuration.ng_annotate.process
-    end
-
-    def parse_opt
-      opt = {}
-      opt_str = ENV['NG_OPT']
-      if opt_str
-        opt = Hash[opt_str.split(',').map { |e| e.split('=') }]
-        opt.symbolize_keys!
-      end
-      if ENV['NG_REGEXP']
-        opt[:regexp] = ENV['NG_REGEXP']
-      end
-      opt
+      process_data(input)
     end
   end
 end
