86a2c2beaca88d0893a08096a3a2a226e7b9259e
Kari Ikonen <mr.kari.ikonen@gmail.com>
2017-08-16 22:01:03 +0300

    initial rails 5.1.x tests

---
+++ test/rails_5.1.x/spec/requests/annotate_js_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe 'annotate js' do
+  let(:expected_content) {
+<<EOF
+var app = angular.module('myApp', []);
+
+app.config(["$locationProvider", function($locationProvider) {
+  $locationProvider.setHtml5Mode(true);
+}]);
+
+app.controller('MainController', ["$scope", function($scope) {}]);
+
+app.directive('myDirective', ["$location", function($location) {
+  return {
+    restrict: 'EA',
+    controller: ["$scope", function($scope) {}]
+  };
+}]);
+
+app.provider('Service', ["$location", function($location) {
+  this.$get = ["$window", function($window) {}];
+}]);
+
+app.provider('OtherService', {
+  $get: ["$window", function($window) {}]
+});
+
+app.run(["$window", function($window) {
+  $window.alert('test!');
+}]);
+EOF
+  }
+
+  it 'annotate js' do
+    get '/assets/angular-app-js.js'
+
+   expect(response.body).to eq expected_content
+  end
+end
--- travis/run.sh
+++ travis/run.sh
@@ -1,8 +1,8 @@
 echo "VER = ${VER}"
-if [[ ${VER} == '4_2' ]]; then
-    cd test/rails_4.2.x
-elif [[ ${VER} == '5_1' ]]; then
+if [[ ${VER} == '5_1' ]]; then
     cd test/rails_5.1.x
+elif [[ ${VER} == '4_2' ]]; then
+    cd test/rails_4.2.x
 elif [[ ${VER} == '3_2' ]]; then
     cd test/example
 fi
