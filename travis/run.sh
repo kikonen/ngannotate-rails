af5703aec9f24366fa1ab0668b31ff9903f8838a
Kari Ikonen <mr.kari.ikonen@gmail.com>
2017-08-16 21:39:45 +0300

    Try new test matrix for travis

--- travis/run.sh
+++ travis/run.sh
@@ -1,6 +1,8 @@
-echo "SPROCKETS3 = ${SPROCKETS3}"
-if [[ ${SPROCKETS3} == true ]]; then
+echo "VER = ${VER}"
+if [[ ${VER} == '4_2' ]]; then
     cd test/rails_4.2.x
+else if [[ ${VER} == '5_1' ]]; then
+    cd test/rails_5.1.x
 else
     cd test/example
 fi
