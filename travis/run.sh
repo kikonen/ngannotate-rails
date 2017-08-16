b063fcbe8eaabb03e9815cae38532a4c93a128c5
Kari Ikonen <mr.kari.ikonen@gmail.com>
2017-08-16 21:46:26 +0300

    travis

--- travis/run.sh
+++ travis/run.sh
@@ -3,7 +3,7 @@ if [[ ${VER} == '4_2' ]]; then
     cd test/rails_4.2.x
 else if [[ ${VER} == '5_1' ]]; then
     cd test/rails_5.1.x
-else
+else if [[ ${VER} == '3_2' ]]; then
     cd test/example
 fi
 echo $PWD
